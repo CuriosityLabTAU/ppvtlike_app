71d22a1b52e97267de5c6faba632903d47a55fa8
Goren Gordon <goren@gorengordon.dom>
2019-03-14 17:08:27 +0200

    for pilot curiosity robotics

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import json
-
+from kivy.core.audio import SoundLoader
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 
@@ -9,6 +9,9 @@ from kivy_communication import *
 from text_handling.text_handling import *
 
 import random
+
+image_path = 'images/ver 2/'
+sounds_path = 'sounds/'
 
 
 class ZeroScreen(Screen):
@@ -32,21 +35,25 @@ class QuestionScreen(Screen):
     def on_enter(self, *args):
         KL.log.insert(action=LogAction.data, obj='screen_question+' + str(self.current_question), comment='enter_screen')
         if self.the_text:
-            TTS.speak([self.the_text])
+            sound_file = sounds_path + 'this is a %s.wav' % self.the_text
+            print(sound_file)
+            the_sound = SoundLoader.load(sound_file)
+            the_sound.play()
+                # TTS.speak([self.the_text])
 
     def update_question(self, current_question=None):
         image_sequence = [0, 1, 2, 3]
         random.shuffle(image_sequence)
 
-        self.ids['A_button'].background_normal = 'images/' + str(self.the_images[image_sequence[0]])
-        self.ids['B_button'].background_normal = 'images/' + str(self.the_images[image_sequence[1]])
-        self.ids['C_button'].background_normal = 'images/' + str(self.the_images[image_sequence[2]])
-        self.ids['D_button'].background_normal = 'images/' + str(self.the_images[image_sequence[3]])
+        self.ids['A_button'].background_normal = image_path + str(self.the_images[image_sequence[0]])
+        self.ids['B_button'].background_normal = image_path + str(self.the_images[image_sequence[1]])
+        self.ids['C_button'].background_normal = image_path + str(self.the_images[image_sequence[2]])
+        self.ids['D_button'].background_normal = image_path + str(self.the_images[image_sequence[3]])
 
-        self.ids['A_button'].background_disabled_down = 'images/' + str(self.the_images[image_sequence[1]])
-        self.ids['B_button'].background_disabled_down = 'images/' + str(self.the_images[image_sequence[2]])
-        self.ids['C_button'].background_disabled_down = 'images/' + str(self.the_images[image_sequence[3]])
-        self.ids['D_button'].background_disabled_down = 'images/' + str(self.the_images[image_sequence[0]])
+        self.ids['A_button'].background_disabled_down = image_path + str(self.the_images[image_sequence[1]])
+        self.ids['B_button'].background_disabled_down = image_path + str(self.the_images[image_sequence[2]])
+        self.ids['C_button'].background_disabled_down = image_path + str(self.the_images[image_sequence[3]])
+        self.ids['D_button'].background_disabled_down = image_path + str(self.the_images[image_sequence[0]])
 
         # because log goes after this, the name is changed to (real number - 1)
         self.ids['A_button'].name = str(self.current_question) + '_A_' + self.the_images[image_sequence[0]][:-4] + '_' + str(image_sequence[0])
@@ -72,6 +79,7 @@ class PpvtLikeApp(App):
         self.init_communication()
 
         TTS.start()
+
         self.sm = ScreenManager()
 
         self.zero_screen = ZeroScreen(name='zero_screen')
@@ -81,11 +89,12 @@ class PpvtLikeApp(App):
         self.questions = []
 
         for exp_name, questions in self.q_dict.items():
-            questions_id = questions.keys()
-            random.shuffle(questions_id)
+            questions_id = [str(k) for k in sorted([int(k) for k in questions.keys()])]
+            # random.shuffle(questions_id) % Do not need to shuffle, same for all children
             self.first_question[exp_name] = questions_id[0]
             for i_question, q_number in enumerate(questions_id):
                 q_data = questions[q_number]
+                print(q_data['text'])
                 self.questions.append(QuestionScreen(name='question_screen_' + q_number))
                 self.questions[-1].exp_name = exp_name
                 self.questions[-1].the_text = q_data['text']
@@ -107,7 +116,7 @@ class PpvtLikeApp(App):
         return self.sm
 
     def load_questions(self):
-        self.q_dict = json.load(open('images/questions.json'))
+        self.q_dict = json.load(open(image_path + 'questions.json'))
 
     def init_communication(self):
         KC.start(the_ip='192.168.1.254', the_parents=[self])  # 127.0.0.1
@@ -117,7 +126,7 @@ class PpvtLikeApp(App):
     def on_connection(self):
         KL.log.insert(action=LogAction.data, obj='SpatialSkillAssessmentApp', comment='start')
 
-    def press_start(self, which_exp='experiment 1'):
+    def press_start(self, which_exp='experiment_1'):
         self.sm.current = 'question_screen_' + self.first_question[which_exp]
 
     def end_game(self):
